55f64d88292f119f8ea901ca9dfadce82b195278
HaroldSP <george.st.petersburg@gmail.com>
2023-02-14 20:48:49 +0300

    Lesson 14 task

--- js/script.js
+++ js/script.js
@@ -4,37 +4,25 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-// В index.html код менять нельзя, только подключите скрипт
-=======
-let title = document.getElementsByTagName('h1')[0];
-let buttonPlus = document.querySelector('.screen-btn');
-let otherItemsPercent = document.querySelectorAll('.other-items.percent');
-let otherItemsNumber = document.querySelectorAll('.other-items.number');
-=======
-// 1) Запретить нажатие кнопки Рассчитать если не выбран ни один тип экрана в выпадающем списке и не введено их количество.
-// Учесть что блоков с типом экранов может быть несколько, но пустых (незаполненных) элементов быть не должно
->>>>>>> 7b0f0da (after practice before HW start (task))
+// 1) Перевести весь проект на стрелочные функции (кроме методов объекта)
 
-// 2) Повесить на input[type=range] (в блоке с классом .rollback) обработчик события.
-// При перемещении ползунка значение под ним (в элементе span) должно меняться.
-// А так же это значение должно заноситься в свойство rollback нашего объекта для последующих расчетов!
+// 2) В нашем объекте везде использовать this как ссылку на объект appData. Если какой то обработчик ломает контекст - привязать нужный контекст в виде объекта appData.
 
-// 3) В нашем объекте присутствует метод getServicePercentPrice.
-// Данный метод рассчитывает доход с учетом отката посреднику.
-// Перенести его логику в метод addPrices и выводить в поле с подписью "Стоимость с учетом отката"
+// 3) Блокировать (свойство disabled) все input[type=text] и select с левой стороны после нажатия кнопки Рассчитать, после этого кнопка Рассчитать пропадает и появляется кнопка Сброс (id=reset)
 
-=======
->>>>>>> 0a4cd9d (main task done)
-// 4) В методе addScreens мы добавляем в свойство appData.screens новые объекты.
-// Добавить свойство count в которое занести количество экранов из input.
-// В методе addPrices посчитать общее количество экранов и вывести на страницу итоговое значение в поле с подписью "Количество экранов"
+// 4) В объекте реализовать метод reset(), срабатывающий по нажатию на кнопку Сброс. Метод reset() должен привести объект к исходному состоянию:
 
-=======
->>>>>>> 37fc953 (+additional task (live rollback values))
+// Кнопка Сброс должна замениться на кнопку Рассчитать
+// Должны быть убраны все дополнительные элементы (которые добавлялись динамически) и значения полей ввода
+// Все input[type=text] и select должны быть разблокированы
+
+// Метод reset должен всю программу возвращать в исходное состояние
+// Метод reset() пишем самостоятельно, никаких перезагрузок страницы. Метод должен быть расписан наподобие start().
+
+// Проверить, чтобы все работало и не было ошибок в консоли
+
+// Добавить папку с уроком на свой GitHub
+
 const title = document.getElementsByTagName('h1')[0];
 const buttonPlus = document.querySelector('.screen-btn');
 const otherItemsPercent = document.querySelectorAll('.other-items.percent');
@@ -53,51 +41,7 @@ let fullTotalCount_fullPrice = document.getElementsByClassName('total-input')[3]
 let totalCountRollback = document.getElementsByClassName('total-input')[4];
 
 let screens = document.querySelectorAll('.screen');
->>>>>>> 8ff44b3 (Start of the practice)
 
-<<<<<<< HEAD
-// Используя только файл скрипта выполнить такие действия:
-
-// Удалить рекламу со страницы
-
-const adv = document.querySelector('.adv');
-adv.remove();
-
-// Восстановить порядок книг
-
-let books = document.querySelectorAll('.book');
-
-books[1].after(books[0]);
-books[0].after(books[4]);
-books[4].after(books[3]);
-books[3].after(books[5]);
-
-books = document.querySelectorAll('.book');
-
-// Заменить картинку заднего фона на другую из папки image
-
-let body = document.querySelector('body');
-body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
-
-// Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
-
-let bookHeaders = document.querySelectorAll('[target="_blank"]')
-
-bookHeaders[2].innerHTML = 'Книга 3. this и Прототипы Объектов';
-
-// Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
-
-// 5th book
-
-let contents = document.querySelectorAll('ul');
-contents[4].classList.add('contents_5');
-let chapters = document.querySelectorAll('ul.contents_5 > li');
-
-chapters[2].before(chapters[9]);
-chapters[9].after(chapters[3]);
-chapters[3].after(chapters[4]);
-chapters[7].after(chapters[5]);
-=======
 const appData = {
   title: '',
   screens: [],
@@ -238,32 +182,7 @@ const appData = {
       return 'Точно что-то пошло не так';
     }
   },
->>>>>>> 7b0f0da (after practice before HW start (task))
 
-<<<<<<< HEAD
-chapters = document.querySelectorAll('ul.contents_5 > li');
-
-// 2nd book
-
-contents[1].classList.add('contents_2');
-let chaptersSecond = document.querySelectorAll('ul.contents_2 > li');
-
-chaptersSecond[9].before(chaptersSecond[2]);
-chaptersSecond[3].after(chaptersSecond[6]);
-chaptersSecond[6].after(chaptersSecond[8]);
-chaptersSecond[7].after(chaptersSecond[9]);
-
-chaptersSecond = document.querySelectorAll('ul.contents_2 > li');
-
-// в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
-
-contents[5].classList.add('contents_6');
-let chaptersSix = document.querySelectorAll('ul.contents_6 > li');
-
-let chapterEight = document.createElement('li');
-chapterEight.textContent = 'Глава 8: За пределами ES6';
-chaptersSix[8].after(chapterEight);
-=======
   logger: function () {
     console.log(appData.fullPrice);
     console.log(appData.servicePercentPrice);
@@ -272,9 +191,4 @@ chaptersSix[8].after(chapterEight);
   }
 }
 
-<<<<<<< HEAD
-appData.start();
->>>>>>> 8ff44b3 (Start of the practice)
-=======
 appData.init();
->>>>>>> 7b0f0da (after practice before HW start (task))
